c33acded351130cf2e7e8880af505a119db22ba2
sofiakopr <sofiakoprcina@gmail.com>
2026-05-22 20:46:21 -0300

    Add files via upload

---
+++ Ejercicio-3-POO-main/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+using Ejercicio3Clases;
+
+namespace Ejercicio_3_POO
+{   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Jugador Jugadorr = new Jugador();
+            Jugadorr.correr(false, true);
+            Jugadorr.descansar();
+
+        }
+    }
+}
