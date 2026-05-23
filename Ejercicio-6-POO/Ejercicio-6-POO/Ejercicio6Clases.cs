34cf07e0686f1de6be242b775fc899b37710fdb6
sofiakopr <sofiakoprcina@gmail.com>
2026-05-22 21:52:56 -0300

    Add files via upload

---
+++ Ejercicio-6-POO/Ejercicio-6-POO/Ejercicio6Clases.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JuegoCartas
+{
+    class Carta
+    {
+        public string Palo { get; }
+        public string Valor { get; }
+
+        public Carta(string palo, string valor)
+        {
+            Palo = palo;
+            Valor = valor;
+        }
+
+        public override string ToString()
+        {
+            return $"{Valor} de {Palo}";
+        }
+    }
+
+    class Mazo
+    {
+        private List<Carta> cartas;
+        private Random random = new Random();
+
+        public Mazo()
+        {
+            cartas = new List<Carta>();
+
+            string[] palos = { "Espadas", "Bastos", "Oros", "Copas" };
+            string[] valores =
+            {
+                "1", "2", "3", "4", "5", "6", "7",
+                "10", "11", "12"
+            };
+
+            foreach (string palo in palos)
+            {
+                foreach (string valor in valores)
+                {
+                    cartas.Add(new Carta(palo, valor));
+                }
+            }
+        }
+
+        public void Barajar()
+        {
+            int n = cartas.Count;
+
+            for (int i = 0; i < n; i++)
+            {
+                int j = random.Next(i, n);
+
+                Carta temp = cartas[i];
+                cartas[i] = cartas[j];
+                cartas[j] = temp;
+            }
+        }
+
+        public Carta RobarCarta()
+        {
+            if (cartas.Count == 0)
+            {
+                throw new InvalidOperationException("El mazo está vacío.");
+            }
+
+            Carta carta = cartas[0];
+            cartas.RemoveAt(0);
+
+            return carta;
+        }
+
+        public int CuantasCartasQuedan()
+        {
+            return cartas.Count;
+        }
+    }
+
+    class Mano
+    {
+        private List<Carta> cartas;
+
+        public Mano()
+        {
+            cartas = new List<Carta>();
+        }
+
+        public void RecibirCarta(Carta carta)
+        {
+            cartas.Add(carta);
+        }
+
+        public void MostrarMano()
+        {
+            Console.WriteLine("Cartas en mano:");
+
+            foreach (Carta carta in cartas)
+            {
+                Console.WriteLine(carta);
+            }
+
+            Console.WriteLine();
+        }
+
+        public int CantidadDeCartas()
+        {
+            return cartas.Count;
+        }
+    }
+}
---
+++ Ejercicio-6-POO/Ejercicio-6-POO/Program.cs
@@ -0,0 +1,32 @@
+﻿using JuegoCartas;
+namespace Ejercicio_6_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mazo mazo = new Mazo();
+
+            mazo.Barajar();
+
+            Mano jugador1 = new Mano();
+            Mano jugador2 = new Mano();
+
+            for (int i = 0; i < 3; i++)
+            {
+                jugador1.RecibirCarta(mazo.RobarCarta());
+                jugador2.RecibirCarta(mazo.RobarCarta());
+            }
+
+            Console.WriteLine("Jugador 1:");
+            jugador1.MostrarMano();
+
+            Console.WriteLine("Jugador 2:");
+            jugador2.MostrarMano();
+
+            Console.WriteLine("Cartas restantes en el mazo: " + mazo.CuantasCartasQuedan());
+            
+        }
+    }
+
+}
