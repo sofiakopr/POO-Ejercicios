499b754bba8b569656fae888e31129797f09a17b
sofiakopr <sofiakoprcina@gmail.com>
2026-05-22 22:37:21 -0300

    Add files via upload

---
+++ Ejercicio 2 POO/Ejercicio 2 POO/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace Ejercicio_2_POO
+{
+
+    public class Cronometro
+    {
+        public int segundos = 0;
+        public int minutos = 0;
+
+        public void reiniciar() {
+            segundos = 0;
+            minutos = 0;
+        }
+
+        public void incrementarTiempo()
+        {
+            if (segundos == 60)
+            {
+                minutos += 1;
+                segundos = 0;
+            }
+            else
+            {
+                segundos += 1;
+            }
+        }
+
+        public void mostrarTiempo()
+        {
+            Console.WriteLine(minutos + "minutos," + segundos + "segundos");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cronometro cron = new Cronometro();
+
+            for(int i = 0; i < 67; i++)
+            {
+                cron.mostrarTiempo();
+                cron.incrementarTiempo();
+            }
+        }
+    }
+}
