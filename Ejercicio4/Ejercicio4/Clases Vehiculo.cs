f78b29888ee2dc17b5c960886efafe68ad1f9321
sofiakopr <sofiakoprcina@gmail.com>
2026-05-23 13:09:23 -0300

    Add files via upload

---
+++ Ejercicio4/Ejercicio4/Clases Vehiculo.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace Ejercicio4Clases
+{
+
+    public interface Vehiculo
+    {
+        public void mover(int segs);
+        public void posicion();
+        public int reiniciarPosicion();
+    }
+
+
+
+    public class Auto : Vehiculo
+    {
+        public int pos = 0;
+        public int velozmax = 40;
+        public int velozactual;
+
+        public Auto(int velocidad)
+        {
+            this.velozactual = velocidad;
+        }
+
+        public void mover(int segs)
+        {
+            if (velozactual > 40) {
+                velozmax = velozactual;
+                pos += velozmax * segs;
+                Console.WriteLine("El auto avanzó" + pos + "metros.");
+            }
+            else
+            {
+                pos += velozmax * segs;
+                Console.WriteLine("El auto avanzó" + pos + "metros.");
+            }
+        }
+
+        public void posicion()
+        {
+            Console.WriteLine("La posicion del auto es: " + pos);
+        }
+
+        public int reiniciarPosicion()
+        {
+            return pos = 0;
+        }
+    }
+
+
+    public class Bicicleta : Vehiculo
+    {
+        public int pos = 0;
+        public int velozmax;
+
+        public Bicicleta()
+        {
+            this.velozmax = 10;
+        }
+
+        public void mover(int segs)
+        {
+            pos += velozmax * segs;
+            Console.WriteLine("La bicicleta avanzó" + pos + "metros.");
+
+        }
+
+        public void posicion()
+        {
+            Console.WriteLine("La posicion de la bicicleta es: " + pos);
+        }
+
+        public int reiniciarPosicion()
+        {
+            return pos = 0;
+        }
+    }
+
+    public class Camion : Vehiculo
+    {
+        public int pos = 0;
+        public int velozmax;
+
+        public Camion()
+        {
+            this.velozmax = 30;
+        }
+
+        public void mover(int segs)
+        {
+            pos += velozmax * segs;
+            Console.WriteLine("El camion avanzó" + pos + "metros.");
+        }
+
+        public void posicion()
+        {
+            Console.WriteLine("La posicion del camion es: " + pos);
+        }
+
+        public int reiniciarPosicion()
+        {
+            return pos = 0;
+        }
+    }
+
+    public class Carrera
+    {
+        public Carrera() { };
+        public void correr(Vehiculo vehiculo1, Vehiculo vehiculo2, int segundos)
+        {
+            vehiculo1.reiniciarPosicion();
+            vehiculo2.reiniciarPosicion();
+
+            if(vehiculo1.mover > vehiculo2.mover)
+            {
+                Console.WriteLine("El/La" + vehiculo1 + "ganó la carrera!");
+            }
+            else if(vehiculo1.mover == vehiculo2.mover)
+            {
+                Console.WriteLine("Empate! Nadie ganó la carrera.");
+            }
+            else
+            {
+                Console.WriteLine("El/La" + vehiculo2 + "ganó la carrera!");
+            }
+        }
+
+    }
+
+}
---
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+using Ejercicio4Clases;
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto Fiat = new Auto(45);
+            Bicicleta bici = new Bicicleta();
+            Camion camion = new Camion();
+            bici.mover(20);
+            bici.posicion();
+            bici.mover(30);
+            bici.posicion();
+            Carrera carrera = new Carrera();
+            carrera.correr(Fiat, bici, 30);
+
+        }
+    }
+}
